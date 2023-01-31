9d453aa30a43b13e365469d786f7f6a3bf07c09a
五月的风 <578595193@qq.com>
2023-01-31 16:24:47 +0800

    FEAT: 实现分页

--- os/src/lib.rs
+++ os/src/lib.rs
@@ -9,6 +9,7 @@ pub mod vga_buffer;
 pub mod serial;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
 use core::panic::PanicInfo;
 
 pub fn init() {
@@ -67,10 +68,13 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     hlt_loop();
 }
 
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+#[cfg(test)]
+entry_point!(test_kernel_main);
 /// Entry point for `cargo test`
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
 
--- os/src/main.rs
+++ os/src/main.rs
@@ -7,15 +7,48 @@
 
 use core::panic::PanicInfo;
 use os::println;
+use bootloader::{BootInfo, entry_point};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     os::init();
     println!("Hello World{}", "!");
+
+    // use os::memory;
+    // use os::memory::BootInfoFrameAllocator;
+    // use x86_64::{structures::paging::{Page, Translate}, VirtAddr};
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    // let mut frame_allocator = unsafe {
+    //     BootInfoFrameAllocator::init(&boot_info.memory_map)
+    // };
+    // let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    //
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let mapper = unsafe { memory::init(phys_mem_offset) };
+    // let addresses = [
+    //     // the identity-mapped vga buffer page
+    //     0xb8000,
+    //     // some code page
+    //     0x201008,
+    //     // some stack page
+    //     0x0100_0020_1a10,
+    //     // virtual address mapped to physical address 0
+    //     boot_info.physical_memory_offset,
+    // ];
+    // for &address in &addresses {
+    //     let virt = VirtAddr::new(address);
+    //     let phys = mapper.translate_addr(virt);
+    //     println!("{:?} -> {:?}", virt, phys);
+    // }
 
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     os::hlt_loop();
 }
 
